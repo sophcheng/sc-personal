7014d35556c2c6f91b3e7387ca67ad419c586449
sophcheng <sophiecheng181@gmail.com>
2025-07-07 00:34:25 -0400

    Added About, Sophware, Homepage tto personal site.

---
+++ index.js
@@ -0,0 +1,5 @@
+const sophware = document.getElementById("sophware");
+const art = document.getElementById("art");
+const about = document.getElementById("about");
+
+// Work in progress -- plan to swap inline mouseover/out logic in HTML for clearer JS
