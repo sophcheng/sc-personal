938744b94465f8ff4fccc4bd69e9dd06dfa4f262
sophcheng <sophiecheng181@gmail.com>
2025-08-05 01:10:30 -0400

    TODO: Polish hover func logic, nix in-line mouse event image switching

--- index.js
+++ index.js
@@ -1,5 +1,52 @@
-const sophware = document.getElementById("sophware");
-const art = document.getElementById("art");
-const about = document.getElementById("about");
+const containers = {
+    sophware: "sophware-container",
+    design: "design-container",
+    about: "about-container"
+};
 
-// Work in progress -- plan to swap inline mouseover/out logic in HTML for clearer JS
+const portalElements = {};
+
+// Get nav portals & children
+for (const [key, id] of Object.entries(containers)){
+    const container = document.getElementById(id);
+    portalElements[key] = {
+        container: container,
+        title: container.querySelector('h2')
+    };
+}
+
+const updateOnHover = (div) => {
+    console.log(`Hover on: ${div}`);
+    portalElements[div].title.style.color = "bisque";
+    portalElements[div].container.style.backgroundColor = "navy";
+}
+
+const resetHover = (div) => {
+    console.log(`Mouse out from: ${div}`);
+    portalElements[div].title.style.color = "#873d73";
+    portalElements[div].container.style.backgroundColor = "bisque";
+}
+
+for (const key in portalElements) {
+    const { container } = portalElements[key];
+    const img = container.querySelector('img');
+    
+    container.addEventListener('mouseover', () => updateOnHover(key));
+    container.addEventListener('mouseout', () => resetHover(key));
+    
+    const originalSrc = img.src;
+    let hoverSrc = '';
+    switch(key) {
+        case 'sophware':
+            hoverSrc = 'imgs/ailuroPlanets.png';
+            break;
+        case 'design':
+            hoverSrc = 'imgs/jinu.png';
+            break;
+        case 'about':
+            hoverSrc = 'imgs/iconAlt.png';
+            break;
+    }
+    img.addEventListener('mouseover', () => { img.src = hoverSrc; });
+    img.addEventListener('mouseout', () => { img.src = originalSrc; });
+}
